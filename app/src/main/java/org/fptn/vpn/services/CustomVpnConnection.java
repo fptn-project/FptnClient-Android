96735cc2cbd2b7898376630f5bd1d61a8a749412
Stas Skokov <7090stas@gmail.com>
2025-09-29 17:58:15 +1000

    fix sending

--- app/src/main/java/org/fptn/vpn/services/CustomVpnConnection.java
+++ app/src/main/java/org/fptn/vpn/services/CustomVpnConnection.java
@@ -55,15 +55,11 @@ public class CustomVpnConnection extends Thread {
     /**
      * Minimum interval between sends
      */
-    public static final long MIN_SEND_INTERVAL_MS = 50;
+    public static final long MIN_SEND_INTERVAL_MS = 5;
     /**
      * Maximum packet size is constrained by the MTU
      */
     private static final int MAX_PACKET_SIZE = 1500;
-    /**
-     * Minimum bytes in buffer to send
-     */
-    public static final int MIN_SIZE_THRESHOLD_TO_SEND = MAX_PACKET_SIZE / 2;
 
     @Getter
     private final int connectionId;
@@ -197,30 +193,16 @@ public class CustomVpnConnection extends Thread {
 
             // Packets to be sent are queued in this input stream.
             try (FileInputStream inputStream = new FileInputStream(vpnInterface.getFileDescriptor())) {
-                // Use a buffer to collect multiple packets before sending
-                ByteArrayOutputStream packetBuffer = new ByteArrayOutputStream(MAX_PACKET_SIZE * 2);
                 byte[] byteBuffer = new byte[MAX_PACKET_SIZE];
-
-                long lastSendTime = 0;
-                while (!currentThread.isInterrupted()
-                        && vpnInterface.getFileDescriptor().valid()) {
+                while (!currentThread.isInterrupted() && vpnInterface.getFileDescriptor().valid()) {
                     try {
                         int length = inputStream.read(byteBuffer);
                         if (length > 0) {
                             uploadRate.update(length);
-                            packetBuffer.write(byteBuffer, 0, length);
-
-                            // Only send if enough time has passed since last send
-                            long currentTime = System.currentTimeMillis();
-                            if (currentTime - lastSendTime >= MIN_SEND_INTERVAL_MS
-                                    || packetBuffer.size() > MIN_SIZE_THRESHOLD_TO_SEND) {
-                                lastSendTime = currentTime;
-                                webSocketClient.send(packetBuffer.toByteArray());
-                                packetBuffer.reset();
-                            }
+                            webSocketClient.send(byteBuffer);
                         } else {
                             // if read buffer empty - sleep
-                            Log.d(getTag(), "Read zero from vpn interface. Sleep...");
+                            //Log.d(getTag(), "Read zero from vpn interface. Sleep...");
                             Thread.sleep(MIN_SEND_INTERVAL_MS);
                         }
                     } catch (Exception e) {
