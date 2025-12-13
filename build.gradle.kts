8adc01de38d118a35211132138b4f9a56e48d822
Stas Skokov <skokov.stanislav1992@gmail.com>
2025-12-13 20:12:37 +1100

    fix sponsors list

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ data class VersionInfo(
 
 fun parseVersionTag(versionTag: String?): VersionInfo {
     if (versionTag.isNullOrBlank()) {
-        return VersionInfo(0, 0, 0, 1)
+        return VersionInfo(1, 2, 21, 2)
     }
     val cleanTag = versionTag.removePrefix("v")
     val parts = cleanTag.split(".").mapNotNull { it.toIntOrNull() }
