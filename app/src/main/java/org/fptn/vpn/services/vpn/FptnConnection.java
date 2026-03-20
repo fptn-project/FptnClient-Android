829f0eeae083fa7b1a9088d091330fa358b7b7bd
Stas Skokov <7090stas@gmail.com>
2026-03-20 20:25:18 +1100

    [feature] add more dns servers (#274)

--- app/src/main/java/org/fptn/vpn/services/vpn/FptnConnection.java
+++ app/src/main/java/org/fptn/vpn/services/vpn/FptnConnection.java
@@ -174,6 +174,11 @@ public class FptnConnection extends Thread {
 
             final String dnsServer = webSocketClient.getDnsServerIPv4();
             builder.addDnsServer(dnsServer);
+            builder.addDnsServer(InetAddress.getByName("1.1.1.1"));
+            builder.addDnsServer(InetAddress.getByName("8.8.8.8"));
+            builder.addDnsServer(InetAddress.getByName("9.9.9.9"));
+            builder.addDnsServer(InetAddress.getByName("216.146.35.35"));
+            builder.addDnsServer(InetAddress.getByName("208.67.222.222"));
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                 builder.excludeRoute(new IpPrefix(InetAddress.getByName(serverEntity.getHost()), 32));
