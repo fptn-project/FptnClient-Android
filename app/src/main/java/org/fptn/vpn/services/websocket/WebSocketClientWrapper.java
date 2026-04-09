247358e5320d53284a269ba9ed76c8ea75b27b76
itachichrist <77534932+itachicoders@users.noreply.github.com>
2026-04-09 21:19:03 +0300

    Refactor getAccessToken to use JSONObject
    Refactor getAccessToken method to use JSONObject for request creation.

--- app/src/main/java/org/fptn/vpn/services/websocket/WebSocketClientWrapper.java
+++ app/src/main/java/org/fptn/vpn/services/websocket/WebSocketClientWrapper.java
@@ -110,9 +110,10 @@ public class WebSocketClientWrapper {
     }
 
     private String getAccessToken() throws PVNClientException {
-        String request = String.format("{\"username\": \"%s\", \"password\": \"%s\"}",
-                serverEntity.getUsername(),
-                serverEntity.getPassword());
+        JSONObject json = new JSONObject();
+        json.put("username", serverEntity.getUsername());
+        json.put("password", serverEntity.getPassword());
+        request = json.toString();
 
         NativeResponse response = nativeHttpsClient.Post(LOGIN_URL, request, 5);
         if (response != null) {
