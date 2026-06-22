6e7853f896fe89175ee24c9bd927897844ebdfdb
Stas Skokov <7090stas@gmail.com>
2026-06-22 19:00:34 +1000

    [feature] license (#327)

--- app/src/main/cpp/src/wrappers/wrapper_websocket_client/wrapper_websocket_client.h
+++ app/src/main/cpp/src/wrappers/wrapper_websocket_client/wrapper_websocket_client.h
@@ -1,9 +1,23 @@
-/*=============================================================================
-Copyright (c) 2024-2025 Stas Skokov
-Copyright (c) 2024-2025 brightsunshine54
+/*
+ * FPTN Android Client
+ * Copyright (C) 2026  Skokov Stanislav, Enin Sergey
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Website: https://fptn.org
+ */
 
-Distributed under the MIT License (https://opensource.org/licenses/MIT)
-=============================================================================*/
 
 #pragma once
 
